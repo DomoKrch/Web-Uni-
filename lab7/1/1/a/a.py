74c7eea9312e1ae047532c3de715b855bba32649
DomoKrch <kyndomo9@gmail.com>
2022-04-01 20:17:50 +0600

    lab7

---
+++ lab7/1/1/a/a.py
@@ -0,0 +1,5 @@
+import math
+
+x, y = [int(z) for z in input().split()]
+z = round((math.sqrt(x**2 + y**2)))
+print(z)
---
+++ lab7/1/1/a/e.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+
+if a > 0:
+    print(abs(109 - a * b))
+else:
+    print(109 + a * b)
